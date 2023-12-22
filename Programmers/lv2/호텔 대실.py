5d7d052d2827e5c7269325d70cb854f3c7b7088b
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-12-22 10:53:03 +0900

    change. lv2 problem in programmers

--- Programmers/lv2/호텔 대실.py
+++ Programmers/lv2/호텔 대실.py
@@ -1,53 +1,41 @@
 def solution(book_time):
     from datetime import datetime, timedelta
+    from collections import deque
 
-    book_time = sorted(book_time, key=lambda x: datetime.strptime(x[0], "%H:%M"))
+    format = "%H:%M"
+    book_time = sorted(book_time, key=lambda x: datetime.strptime(x[0], format))
 
-    available_times = []
+    rooms = deque([])
+    cnt = 0
 
-    for st_time, ed_time in book_time:
-        start = datetime.strptime(st_time, "%H:%M")
-        end = datetime.strptime(ed_time, "%H:%M") + timedelta(minutes=10)
+    for book in book_time:
+        st_time = datetime.strptime(book[0], format)
+        ed_time = datetime.strptime(book[1], format) + timedelta(minutes=10)
 
-        flag = False
-        for i in available_times:
-            if i <= start:
-                i = end
-                flag = True
+        flag = True
+        for index in range(len(rooms)):
+            if rooms[index] < st_time:
+                rooms.remove(rooms[index])
+                rooms.append(ed_time)
+                flag = False
                 break
 
-        if not flag:
-            available_times.append(end)
+        if flag:
+            rooms.append(ed_time)
+            cnt += 1
 
-    return len(available_times)
+    return cnt
 
 
 if __name__ == "__main__":
     print(
         solution(
             [
-                ["09:10", "10:10"],
-                ["09:10", "10:10"],
-                ["10:20", "12:20"],
-                ["10:20", "12:20"],
+                ["15:00", "17:00"],
+                ["16:40", "18:20"],
+                ["14:20", "15:20"],
+                ["14:10", "19:20"],
+                ["18:20", "21:20"],
             ]
         )
     )
-
-    # 2
-    print(solution([["00:01", "00:10"], ["00:19", "00:29"]]))
-
-    # 2
-    print(solution([["08:00", "08:30"], ["08:00", "13:00"], ["12:30", "13:30"]]))
-
-    # 1
-    print(solution([["16:00", "16:10"], ["16:20", "16:30"], ["16:40", "16:50"]]))
-
-    # 2
-    print(solution([["15:00", "17:00"], ["15:00", "17:00"]]))
-
-    # 1
-    print(solution([["09:10", "10:10"], ["10:20", "12:20"], ["12:30", "13:20"]]))
-
-    # 1
-    print(solution([["10:00", "10:10"]]))
