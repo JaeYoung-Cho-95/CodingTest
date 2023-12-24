e4796aa52244e2e6c5efda5147f707e682167b61
JaeYoung_Cho_95 <whwo9745@gmail.com>
2023-12-24 13:55:06 +0900

    add. lv1 problem in programmers

---
+++ Programmers/lv1/숫자 짝궁.py
@@ -0,0 +1,30 @@
+def solution(X, Y):
+    cnt_Y = {key: 0 for key in Y}
+
+    for i in Y:
+        cnt_Y[i] += 1
+
+    answer = []
+    for i in X:
+        try:
+            if cnt_Y[i] > 0:
+                answer.append(str(i))
+                cnt_Y[i] -= 1
+        except:
+            pass
+
+    if len(answer) == 0:
+        return "-1"
+
+    answer = sorted(answer, reverse=True)
+    if answer[0] == '0':
+        return "0"
+    
+    answer = "".join(answer)
+    return answer
+
+
+if __name__ == "__main__":
+    # print(solution("100", "2345"))
+    print(solution("100", "203045"))
+    print(solution("2987654", "1200000"))
