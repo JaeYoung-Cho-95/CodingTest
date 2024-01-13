b6c82806a33cd42cd571ecb896902f99ccc1ca41
JaeYoung_Cho_95 <whwo9745@gmail.com>
2024-01-13 15:15:53 +0900

    add. lv2 problem in programmers.

--- Programmers/lv2/마법의 엘리베이터.py
+++ Programmers/lv2/마법의 엘리베이터.py
@@ -1,21 +1,42 @@
 def solution(storey):
     from collections import deque
-    storey = deque([x for x in str(storey)[::-1]])
+    storey = deque([int(x) for x in str(storey)[::-1]])
     print(storey)
     
     answer = 0
     while storey:
-        temp = int(storey.popleft())
+        temp = storey.popleft()
         
-        if temp >= 6:
-            temp = 10 - temp + 1
+        if temp == 5 and storey:
+            cnt = 0
+            if storey[cnt] >= 5:
+                storey[cnt] += 1
+                while cnt < len(storey):
+                    if storey[cnt] == 10:
+                        storey[cnt] = 0
+                        storey[cnt+1] += 1
+                        cnt += 1
+                    else:
+                        break
             answer += temp
+            
+        elif temp >= 6:
+            answer += (10 - temp)
+            if storey:
+                storey[0] += 1
+            else:
+                answer += 1
         else:
             answer += temp
+        # print(f"answer : {answer}")
+        # print(f"storey : {storey}")
         
     return answer
 
 if __name__ == "__main__":
-    print(solution(16))
-    print(solution(2554))
-    print(solution(24312304810))
+    # print(solution(16))
+    # print(solution(2554))
+    # print(solution(1000000))
+    # print(solution(2999999))
+    print(solution(5999999))
+    print(solution(35))
